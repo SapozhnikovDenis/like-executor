56f80009de2f7c82c666493f9717d5cc747d230e
sapozhnikovdn <sapozhnikov.denis33@gmail.com>
2019-09-28 20:30:47 +0300

    add test for likeExecutor

---
+++ src/main/java/com/sapozhnikov/executor/LikeExecutor.java
@@ -0,0 +1,10 @@
+package com.sapozhnikov.executor;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.Callable;
+
+public class LikeExecutor {
+
+    public void addTask(LocalDateTime timeRunTask, Callable task) {
+    }
+}
