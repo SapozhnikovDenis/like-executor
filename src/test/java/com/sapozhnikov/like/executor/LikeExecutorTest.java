ab60a79d20768f877dfeb72f43c9adfc425b3d3a
Sapozhnikov Denis <sapozhnikov.denis33@gmail.com>
2019-10-05 11:19:51 +0300

    add executor (#1)
    * add test for likeExecutor
    
    * add realization LikeExecutor

---
+++ src/test/java/com/sapozhnikov/like/executor/LikeExecutorTest.java
@@ -0,0 +1,163 @@
+package com.sapozhnikov.like.executor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.Assert.assertTrue;
+
+public class LikeExecutorTest {
+
+    private LikeExecutor likeExecutor;
+
+    @Before
+    public void init() {
+        likeExecutor = new LikeExecutor();
+    }
+
+    @Test
+    public void testAddAndRunTask() throws InterruptedException {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        LocalDateTime timeRunTask = LocalDateTime.now().plusNanos(100000000);
+
+        likeExecutor.addTask(timeRunTask, () -> {
+                    atomicBoolean.set(true);
+                    String message = "completed!";
+                    System.out.println(message);
+                    return message;
+                }
+        );
+
+        TimeUnit.MILLISECONDS.sleep(200);
+        assertTrue(atomicBoolean.get());
+    }
+
+    @Test
+    public void testAddAndRunTwoTask() throws InterruptedException {
+        AtomicBoolean firstTaskTester = new AtomicBoolean(false);
+        AtomicBoolean secondTaskTester = new AtomicBoolean(false);
+        LocalDateTime timeRunFirstTask = LocalDateTime.now().plusNanos(100000000);
+        LocalDateTime timeRunSecondTask = LocalDateTime.now().plusNanos(200000000);
+
+        likeExecutor.addTask(timeRunFirstTask, () -> {
+                    firstTaskTester.set(true);
+                    String message = "completed first task!";
+                    System.out.println(message);
+                    return message;
+                }
+        );
+        likeExecutor.addTask(timeRunSecondTask, () -> {
+                    if (firstTaskTester.get()) {
+                        secondTaskTester.set(true);
+                    }
+                    String message = "completed second task!";
+                    System.out.println(message);
+                    return message;
+                }
+        );
+
+        TimeUnit.MILLISECONDS.sleep(300);
+        assertTrue(firstTaskTester.get());
+        assertTrue(secondTaskTester.get());
+    }
+
+    @Test
+    public void testAddTwoTaskInvertOrderAndRunCorrectOrder() throws InterruptedException {
+        AtomicBoolean firstTaskTester = new AtomicBoolean(false);
+        AtomicBoolean secondTaskTester = new AtomicBoolean(false);
+        LocalDateTime timeRunFirstTask = LocalDateTime.now().plusNanos(100000000);
+        LocalDateTime timeRunSecondTask = LocalDateTime.now().plusNanos(200000000);
+
+        likeExecutor.addTask(timeRunSecondTask, () -> {
+                    if (firstTaskTester.get()) {
+                        secondTaskTester.set(true);
+                    }
+                    String message = "completed second task!";
+                    System.out.println(message);
+                    return message;
+                }
+        );
+        likeExecutor.addTask(timeRunFirstTask, () -> {
+                    firstTaskTester.set(true);
+                    String message = "completed first task!";
+                    System.out.println(message);
+                    return message;
+                }
+        );
+
+        TimeUnit.MILLISECONDS.sleep(300);
+        assertTrue(firstTaskTester.get());
+        assertTrue(secondTaskTester.get());
+    }
+
+    @Test
+    public void testAddAndRunTwoTaskWithEqualsTimeRun() throws InterruptedException {
+        AtomicBoolean firstTaskTester = new AtomicBoolean(false);
+        AtomicBoolean secondTaskTester = new AtomicBoolean(false);
+        LocalDateTime timeRunBothTask = LocalDateTime.now().plusNanos(100000000);
+
+        likeExecutor.addTask(timeRunBothTask, () -> {
+                    firstTaskTester.set(true);
+                    String message = "completed first task!";
+                    System.out.println(message);
+                    return message;
+                }
+        );
+        likeExecutor.addTask(timeRunBothTask, () -> {
+                    if (firstTaskTester.get()) {
+                        secondTaskTester.set(true);
+                    }
+                    String message = "completed second task!";
+                    System.out.println(message);
+                    return message;
+                }
+        );
+
+        TimeUnit.MILLISECONDS.sleep(300);
+        assertTrue(firstTaskTester.get());
+        assertTrue(secondTaskTester.get());
+    }
+
+    @Test
+    public void testAddAndRunTwoTaskWithAddTaskFromDifferentThreads() throws InterruptedException {
+        AtomicBoolean firstTaskTester = new AtomicBoolean(false);
+        AtomicBoolean secondTaskTester = new AtomicBoolean(false);
+        LocalDateTime timeRunFirstTask = LocalDateTime.now().plusNanos(100000000);
+        LocalDateTime timeRunSecondTask = LocalDateTime.now().plusNanos(200000000);
+
+        Thread threadForAddFirstTask = new Thread(() ->
+                likeExecutor.addTask(timeRunFirstTask, () -> {
+                            firstTaskTester.set(true);
+                            String message = "completed first task!";
+                            System.out.println(message);
+                            return message;
+                        }
+                ));
+        Thread threadForAddSecondTask = new Thread(() ->
+                likeExecutor.addTask(timeRunSecondTask, () -> {
+                            if (firstTaskTester.get()) {
+                                secondTaskTester.set(true);
+                            }
+                            String message = "completed second task!";
+                            System.out.println(message);
+                            return message;
+                        }
+                ));
+        threadForAddSecondTask.start();
+        threadForAddFirstTask.start();
+
+        TimeUnit.MILLISECONDS.sleep(300);
+        assertTrue(firstTaskTester.get());
+        assertTrue(secondTaskTester.get());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNotAddOutdatedTask() {
+        LocalDateTime timeRunTask = LocalDateTime.now().minusSeconds(1);
+
+        likeExecutor.addTask(timeRunTask, null);
+    }
+}
